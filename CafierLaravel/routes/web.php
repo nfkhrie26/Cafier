1aef653b5ca2d30fa3a2d9f15ad3163db45583ea
osanna patricia <osannapatricia@gmail.com>
2026-06-03 06:07:15 +0700

    apa

--- CafierLaravel/routes/web.php
+++ CafierLaravel/routes/web.php
@@ -26,29 +26,9 @@ Route::get('/keuangan', function () {
     return redirect('http://127.0.0.1:8001/pengeluaran-pemasukan');
 });
 
-<<<<<<< HEAD
-// RUTE DASHBOARD & HALAMAN STATIS
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::get('/daftar-membership', function () {
-    return view('daftar-membership');
-})->name('membership.index');
-
-Route::get('/daftar-karyawan', function () {
-    return view('daftar-karyawan');
-})->name('karyawan.index');
-
-// RUTE MENU KAFE (Diurus oleh MenuController)
-Route::get('/daftar-menu', [MenuController::class, 'index'])->name('menu.index');
-Route::post('/pemilik/menu/store', [MenuController::class, 'store'])->name('menu.store');
-Route::put('/pemilik/menu/update/{id}', [MenuController::class, 'update'])->name('menu.update');
-Route::get('/keuangan', [KeuanganController::class, 'index'])->name('keuangan.index');
-
-=======
 Route::get('/dashboard', function () {
     return redirect('http://127.0.0.1:8001/dashboard');
 });
->>>>>>> f6761fdcc1b9f65da979757bd830d7fb15a3e555
 
 // RUTE API (Untuk Aplikasi Mobile)
 Route::apiResource('categories', CategoryController::class);
--- CafierMobile/service/utils.ts
+++ CafierMobile/service/utils.ts
@@ -3,12 +3,12 @@ import { router } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
 // import { getItemAsync, deleteItemAsync } from './storage';
 
-export const IMAGE_BASE_URL = 'https://trinity-milliary-mitzie.ngrok-free.dev/storage/';
+export const IMAGE_BASE_URL = 'https://posttetanic-latanya-unemanative.ngrok-free.dev/storage/';
 
 // 1. Bikin instance (kurir khusus buat Cafier)
 const api = axios.create({
   // SKEPTIS ALERT: Pastiin IP lu belom ganti ya!
-  baseURL: 'https://trinity-milliary-mitzie.ngrok-free.dev/ApiMobile', 
+  baseURL: 'https://posttetanic-latanya-unemanative.ngrok-free.dev/ApiMobile', 
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
